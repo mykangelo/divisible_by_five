5439b0d029354fb82dbfe07ac7315894d859f7f0
mykangelo <mmykangelo@gmail.com>
2024-01-22 16:52:25 +0800

    Answer for exercise number 6

---
+++ exercise6.py
@@ -0,0 +1,18 @@
+
+
+
+#store the given in the varaible first
+given_list = [10, 20, 33, 46, 55]
+
+#print the givne list and where it is need to be divisible of
+print("Given list is", given_list)
+print("Divisible by 5")
+
+#create a for loop and iterate it to the given list
+for i in given_list:
+    if (i % 5) == 0:
+        print(i)
+
+
+
+        
